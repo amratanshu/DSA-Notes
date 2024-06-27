e16f0c2de8f604983cabf909e0421b5064d4b19c
Apple <amratanshu99@gmail.com>
2024-06-28 03:01:17 +0530

    Add KMP 2 edit

--- String/Minimum Characters required to make a String Palindromic (HARD for me - uses LPS from KMP).cpp
+++ String/Minimum Characters required to make a String Palindromic (HARD for me - uses LPS from KMP).cpp
@@ -10,3 +10,41 @@ them and compute the LPS array of this combined string. Now take the last value 
 subtract it with the length of the original string, This will give us the minimum number of insertions 
 required in the begining of the string .
 
+Look at the examples - you will understand easily
+
+Code is really easy as it is just LPS KMP code which is difficult to know.
+
+vector<int> preprocessing(string pat) {
+    int M = pat.length();
+
+    vector<int> lps(M, 0);
+    lps[0] = 0;
+    int i = 1, len = 0;
+    while (i < M) {
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else
+        {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else // if (len == 0)
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps;
+}
+int Solution::solve(string A) {
+    if (A.length() == 1)
+        return 0;
+    
+    string copyA = A;
+    reverse(A.begin(), A.end());
+    vector<int> lps = preprocessing(copyA + '$' + A);
+    
+    return A.length() - lps.back();
+}
