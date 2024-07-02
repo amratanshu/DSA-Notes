456ae52ab1bcfea3cc877448447a73e362cfc945
Apple <amratanshu99@gmail.com>
2024-07-03 00:24:44 +0530

    changes

--- Binary Tree/Balanced Binary Tree.cpp
+++ Binary Tree/Balanced Binary Tree.cpp
@@ -62,46 +62,41 @@ So i propose -
 
 His code - 
 
-bool isBalanced(Node* root) {
-        // Check if the tree's height difference
-        // between subtrees is less than 2
-        // If not, return false; otherwise, return true
-        return dfsHeight(root) != -1;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int solve(TreeNode* root) {
+        if (!root)
+            return true;
+        
+        int lh = solve(root->left);
+        int rh = solve(root->right);
+
+        if (lh == -1 || rh == -1)
+            return -1;
+
+        if (abs(lh - rh) > 1) {
+            return -1;
+        }
+        
+        return 1 + max(lh, rh);
     }
 
-    // Recursive function to calculate
-    // the height of the tree
-    int dfsHeight(Node* root) {
-        // Base case: if the current node is NULL,
-        // return 0 (height of an empty tree)
-        if (root == NULL) return 0;
-
-        // Recursively calculate the
-        // height of the left subtree
-        int leftHeight = dfsHeight(root->left);
-
-        // If the left subtree is unbalanced,
-        // propagate the unbalance status
-        if (leftHeight == -1) 
-            return -1;
-
-        // Recursively calculate the
-        // height of the right subtree
-        int rightHeight = dfsHeight(root->right);
-
-        // If the right subtree is unbalanced,
-        // propagate the unbalance status
-        if (rightHeight == -1) 
-            return -1;
-
-        // Check if the difference in height between
-        // left and right subtrees is greater than 1
-        // If it's greater, the tree is unbalanced,
-        // return -1 to propagate the unbalance status
-        if (abs(leftHeight - rightHeight) > 1)  
-            return -1;
-
-        // Return the maximum height of left and
-        // right subtrees, adding 1 for the current node
-        return max(leftHeight, rightHeight) + 1;
+    bool isBalanced(TreeNode* root) {
+        if (!root)
+            return true;
+        if (solve(root) == -1)
+            return false;
+        return true;
     }
+};
