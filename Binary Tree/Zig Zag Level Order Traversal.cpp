3ae0a550782b3213571deddd40c7b9e3f5f511f2
Apple <amratanshu99@gmail.com>
2024-07-03 16:16:15 +0530

    changes

---
+++ Binary Tree/Zig Zag Level Order Traversal.cpp
@@ -0,0 +1,51 @@
+My code ->
+
+A variable MySwitch decides the pop and push to which side - and also push left or right
+Modification of Level Order Traversal
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (!root) {
+            return {};
+        }
+        deque<TreeNode*> q;
+        bool mySwitch = true; //switches where to pop and push, and the order of left/right pushing
+        q.push_front(root);
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> temp;
+            for (int i = 0; i < n; i++) {
+                TreeNode* it;
+            
+                if (mySwitch) {
+                    it = q.front();
+                    temp.push_back(it->val);
+                    q.pop_front();
+                }
+                else {
+                    it = q.back();
+                    temp.push_back(it->val);
+                    q.pop_back();
+                }
+                TreeNode* left = it->left;
+                TreeNode* right = it->right;
+
+                if (!mySwitch) swap(left, right);
+                if (mySwitch) {
+                    if (left) q.push_back(left);
+                    if (right) q.push_back(right);
+                }
+                else {
+                    if (left) q.push_front(left);
+                    if (right) q.push_front(right);
+                }
+                
+            }
+            mySwitch = !mySwitch;
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
