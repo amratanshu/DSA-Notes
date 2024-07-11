8a0339b6762760ef396377de34877af413627e4c
Apple <amratanshu99@gmail.com>
2024-07-12 00:31:34 +0530

    changes

--- Graphs - 2/Strongly Connected Component - SCC - Kosaraju Algo (HARD).cpp
+++ Graphs - 2/Strongly Connected Component - SCC - Kosaraju Algo (HARD).cpp
@@ -21,3 +21,21 @@ We dont want that. We just want it to cycle in those 3 nodes - 0 1 2
 
 Haina? so what if we reverse the edge from 3 - that way - we block it going from 2 to 3
 
+Similarly - if we reverse the edges of 3->4, 4->7, 6->7 - and we run dfs on each of the nodes - 
+
+we can print the individual SSC's
+
+But how will we know which edges to reverse? yeah we dont. So just reverse all the edges
+
+Also - after doing this - we want to print the SSCs in order.
+
+So for that - after reversing - we also need to know whether to run dfs on 0 first or 2 first or 7 first...
+
+This makes our algorithm !!
+
+1. Do a dfs and store all the elements in the dfs in the stack - this way when u take it out - the elements 
+will be in the order of the time_frame it took to print them - basically sorted oppositely
+
+2. Reverse all the nodes in the graph
+
+3. Now start popping from the stack and start doing dfs on it - if this is not visited yet
