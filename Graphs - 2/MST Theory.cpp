1fc8f74fc1746e618dac479ef79fe230b2b78219
Apple <amratanshu99@gmail.com>
2024-07-28 20:26:03 +0530

    more changes

--- Graphs - 2/MST Theory.cpp
+++ Graphs - 2/MST Theory.cpp
@@ -8,4 +8,3 @@ And all nodes are connected in a single Strongly connected component
 There can be many spanning trees
 
 The sum of the weights of all edges = if that is minimum - it is the munimum spanning tree
-
---
+++ Graphs - 2/PRIM MST Algo.cpp
@@ -0,0 +1,24 @@
+https://www.youtube.com/watch?v=mJcZjjKzeqk&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=45&ab_channel=takeUforward
+
+Take a priority queue
+pq -> <wt, node, parent>
+
+Step 1 ->
+Insert the first node (can be any node) on to the pq
+
+0, 0, -1 --> first insertion into the pq
+
+Step 2 ->
+take the min element out - 
+If its unvisited - 
+mark them as visited
+
+take the adjacent nodes
+
+put them in the pq
+
+
+Untimately u will get the MST
+
+
+
