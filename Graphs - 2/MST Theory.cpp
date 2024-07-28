0c89236a0c7710967ba94de8e7ae20c0a9d4a423
Apple <amratanshu99@gmail.com>
2024-07-28 20:12:11 +0530

    more changes

---
+++ Graphs - 2/MST Theory.cpp
@@ -0,0 +1,11 @@
+A MST is a tree where 
+
+N nodes
+N - 1 edges
+And all nodes are connected in a single Strongly connected component
+
+
+There can be many spanning trees
+
+The sum of the weights of all edges = if that is minimum - it is the munimum spanning tree
+
