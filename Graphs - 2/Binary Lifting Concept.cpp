4373a7b681f08a2e02561c0779bbb24ab374cfa6
Apple <amratanshu99@gmail.com>
2024-07-31 15:42:47 +0530

    more changes

--- Graphs - 2/Binary Lifting Concept.cpp
+++ Graphs - 2/Binary Lifting Concept.cpp
@@ -1,1 +1,6 @@
 https://leetcode.com/problems/checking-existence-of-edge-length-limited-paths-ii/solutions/4364610/minimum-spanning-tree-binary-lifting/
+
+nice explanation: https://www.youtube.com/watch?v=MOy4UDjN8DM
+
+explanation of the GRAPH HARD problem where binary lifting is used -
+https://leetcode.com/problems/checking-existence-of-edge-length-limited-paths-ii/solutions/4563257/mst-path-max-query-using-lca-binary-lifting/
