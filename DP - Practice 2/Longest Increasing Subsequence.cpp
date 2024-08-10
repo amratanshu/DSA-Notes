db984edf538dc79c37d06154b18164169c73f752
Apple <amratanshu99@gmail.com>
2024-08-10 14:34:19 +0530

    more changes

--- DP - Practice 2/Longest Increasing Subsequence.cpp
+++ DP - Practice 2/Longest Increasing Subsequence.cpp
@@ -59,3 +59,36 @@ public:
         return ans == INT_MIN? 1: ans;
     }
 };
+
+
+
+My solution - do a dry run and create the 2D dp table on paper to understand more -
+
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.length();
+        int m = text2.length();
+
+        vector<vector<int>> dp (n, vector<int>(m, 0));
+        int ans = INT_MIN;
+    
+        for(int i=0;i<n; i++) {
+            for (int j=0;j<m;j++) {
+                if (text1[i] == text2[j]) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1 + 0;
+                    }
+                    else dp[i][j] = 1 + dp[i-1][j-1]; //take
+                }
+                else { //not take
+                    if (i == 0 && j) dp[i][j] = dp[i][j-1];
+                    else if (i && j == 0) dp[i][j] = dp[i-1][j];
+                    else if (i && j) dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
