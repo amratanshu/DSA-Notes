158bacde5919b85a18cdc99310f1ceb1369009bc
Apple <amratanshu99@gmail.com>
2024-08-10 15:52:15 +0530

    more changes

--- DP - Practice 2/Target Sum (0-1 knapsack).cpp
+++ DP - Practice 2/Target Sum (0-1 knapsack).cpp
@@ -21,3 +21,30 @@ public:
 
 DP ->
 
+given that the sum of elements is less than = 1000
+so we can shift elements accordingly ->
+
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(2001, 0)); // -1000 to 1000 shifted to 0 to 2000
+
+        // Base case:
+        dp[0][1000] = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int t = -1000; t <= 1000; t++) {
+                int shiftedTarget = t + 1000;
+                if (shiftedTarget - nums[i-1] >= 0 && shiftedTarget - nums[i-1] <= 2000) {
+                    dp[i][shiftedTarget] += dp[i-1][shiftedTarget - nums[i-1]];
+                }
+                if (shiftedTarget + nums[i-1] >= 0 && shiftedTarget + nums[i-1] <= 2000) {
+                    dp[i][shiftedTarget] += dp[i-1][shiftedTarget + nums[i-1]];
+                }
+            }
+        }
+
+        return dp[n][target + 1000];
+    }
+};
