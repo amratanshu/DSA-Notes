0149842e7ebe42789ef7b8d6667c2e92e61438e1
Apple <amratanshu99@gmail.com>
2024-08-10 15:09:10 +0530

    more changes

--- DP - Practice 2/Target Sum (0-1 knapsack).cpp
+++ DP - Practice 2/Target Sum (0-1 knapsack).cpp
@@ -1,2 +1,23 @@
 https://leetcode.com/problems/target-sum/description/
 
+https://leetcode.com/problems/target-sum/solutions/455024/dp-is-easy-5-steps-to-think-through-dp-questions/
+
+BASIC RECURSION ->
+
+class Solution {
+public:
+    int recurse(int index, int target, vector<int>& nums) {
+        if (index == -1 && target == 0) return 1;
+        if (index == -1) return 0;
+
+        return recurse(index-1, target-nums[index], nums) + recurse(index-1, target+nums[index], nums);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        return recurse(n-1, target, nums);
+    }
+};
+
+
+DP ->
+
