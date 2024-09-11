3a10abf2dfe3fafd610ede1f50503f933180c525
Apple <amratanshu99@gmail.com>
2024-09-10 21:54:54 -0400

    more changes

--- DP - Practice 2/Subset sum.cpp
+++ DP - Practice 2/Subset sum.cpp
@@ -1,1 +1,15 @@
-https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1
+https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/
+
+Recursive / Recurrence relation solution ->
+
+public:
+    bool recur(int ind, int sum, vector<int> &arr, int n) {
+        if (ind == n) return false;
+        if (sum == 0) return true;
+        if (recur(ind+1, sum, arr, n) || recur(ind+1, sum - arr[ind], arr, n)) return true;
+    }
+    
+    bool isSubsetSum(vector<int>arr, int sum){
+        // code here 
+        return recur(0, sum, arr, arr.size());
+    }
