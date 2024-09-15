217aef47205d91226f9f816e48dad832068597e6
Apple <amratanshu99@gmail.com>
2024-09-14 22:16:06 -0400

    more changes

---
+++ DP - Practice 2/Partition equal subset sum.cpp
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/partition-equal-subset-sum/
+
+Concept - same as subset sum
+
+we are just trying to find a subset whose sum = total/2
+
+But pay attention to the edge cases - 
+edge case 1. [100]
+edge case 2. total sum is an odd number - simply return false in this case and dont do the dp shiz.
+
--- DP - Practice 2/Subset sum.cpp
+++ DP - Practice 2/Subset sum.cpp
@@ -13,40 +13,36 @@ public:
         // code here 
         return recur(0, sum, arr, arr.size());
     }
+}
 
-
-
-Dp solution draft -
+WATCH STRIVER SOLUTION IN HIS VIDEO - better explanation and better use of indexes and bounds for understanding..
+Dp solution -
 
 public:
-    bool recur(int ind, int sum, vector<int> &arr, int n) {
-        if (ind == n) return false;
-        if (sum == 0) return true;
-        if (recur(ind+1, sum, arr, n) || recur(ind+1, sum - arr[ind], arr, n)) return true;
-    }
+bool isSubsetSum(vector<int>& arr, int sum) {
+    int n = arr.size();
+    vector<vector<bool>> dp(n, vector<bool>(sum + 1, false));
     
-    bool isSubsetSum(vector<int>arr, int sum){
-        // code here 
-        // return recur(0, sum, arr, arr.size());
-        int n = arr.size();
-        int dp[n][sum+1];
-        
-        //base condition 1
-        for (int i=0; i<=sum; i++) {
-            dp[n-1][i] = 0;
-        }
-        
-        //base condition 2
-        for (int i=0;i<n;i++) {
-            dp[i][0] = 1;
-        }
-        
-        for (int ind=n-2; ind>=0; ind--) {
-            
-            for (int k=0; k<=sum; k++) {
-                
-            }
-                
-            }
+    // Base condition: sum 0 can always be achieved by taking an empty subset.
+    for (int i = 0; i < n; i++) {
+        dp[i][0] = true;
+    }
+
+    // Base condition: if we are only allowed to pick the last element
+    if (arr[n-1] <= sum) {
+        dp[n-1][arr[n-1]] = true;
+    }
+
+    // Fill the DP table
+    for (int ind = n-2; ind >= 0; ind--) {
+        for (int k = 1; k <= sum; k++) {
+            // Exclude the current element
+            bool notTake = dp[ind+1][k];
+            // Include the current element (check if it's within bounds)
+            bool take = (k >= arr[ind]) ? dp[ind+1][k - arr[ind]] : false;
+            dp[ind][k] = notTake || take;
         }
     }
+
+    return dp[0][sum];
+}
