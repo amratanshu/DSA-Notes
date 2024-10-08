c3012b463282409a63e1109efc5960e8c0b5b3b6
Apple <amratanshu99@gmail.com>
2024-10-07 20:57:06 -0400

    add more changes

--- DP/Longest Common Substring.cpp
+++ DP/Longest Common Substring.cpp
@@ -5,6 +5,15 @@ SUBSTRING! NOT SUBSEQUENCE!
 
 The ans is just dependent on the last i-1, j-1
 
+For this - remember - dp[i][j] - stores the answer for longest common substring ENDING AT ith and jth positions.
+SO - dp[i][j] will not give us the answer. Rather - the max of all elements in the entire dp array will
+give us the length if the LCS answer.
+
+Hence - Remember what we are storing in dp[i][j] !!
+
+We are not storing the longest common substring till i and j. We are storing the LCS which includes ith and jth characters.
+(Ending at i and j)
+
 When chars are equal 
     dp[i-1][j-1] + 1
 
