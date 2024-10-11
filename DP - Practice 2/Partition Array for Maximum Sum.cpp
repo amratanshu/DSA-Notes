fa3843dd4e5b546d2955198a3193f470b9da2a56
Apple <amratanshu99@gmail.com>
2024-10-11 02:37:00 -0400

    add more changes

--- DP - Practice 2/Partition Array for Maximum Sum.cpp
+++ DP - Practice 2/Partition Array for Maximum Sum.cpp
@@ -33,3 +33,36 @@ public:
     }
 
 };
+
+Memoized easy - 
+
+class Solution {
+public:
+    int solve(int i, int k, vector<int> &arr, vector<int> &dp) {
+        if (i == 0) return dp[i] = 0;
+
+        int maxEle = 0;
+        int ans = 0;
+
+        if (dp[i] != -1) return dp[i];
+
+        for (int j = i; j >= max(1, i - k + 1); j--) {
+            maxEle = max(maxEle, arr[j - 1]);
+            int partitionSize = i - j + 1;
+
+            // Calculate the sum for this partition
+            int sumOfEles = maxEle * partitionSize;
+            int remaining = solve(j - 1, k, arr, dp);
+
+            ans = max(ans, sumOfEles + remaining);
+        }
+        return dp[i] = ans;
+    }
+
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> dp(n+1, -1);
+        return solve(n, k, arr, dp);
+    }
+
+};
