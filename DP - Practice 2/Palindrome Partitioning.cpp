5dcc1f5d7f23401afaa964d88cffd75175c9c2c8
Apple <amratanshu99@gmail.com>
2024-10-11 02:34:57 -0400

    add more changes

--- DP - Practice 2/Palindrome Partitioning.cpp
+++ DP - Practice 2/Palindrome Partitioning.cpp
@@ -113,3 +113,56 @@ Also - as in this DP solution - we use only the dp[k+1][j] wala part - we can ev
 
 How to write the solution now?
 
+UNDERSTAND Better using strivers video on this - how did we go from 2D to 1D in this is still a little unclear - 
+implementation wise
+
+CONCEPT he uses - FRONT PARTITION
+
+There is no need of a second variable.
+
+1. We can just place the partition pointer anywhere.
+2. Check if the front partition is a palindrome
+3. if it is - then partition it there and do - no of cuts =  1 + func(rightPartition)
+
+just return the minimum of all such no. of cuts
+
+class Solution {
+public:
+    bool isPal(int s, int e, string& str, vector<vector<int>>& isPaldp) {
+        if (s >= e) return true;
+
+        if (isPaldp[s][e] != -1) return isPaldp[s][e];
+
+        if (str[s] == str[e]) {
+            return isPaldp[s][e] = isPal(s + 1, e - 1, str, isPaldp);
+        } else {
+            return isPaldp[s][e] = false;
+        }
+    }
+
+    int minCut(string s) {
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        vector<vector<int>> isPaldp(n, vector<int>(n, -1));
+
+        // base case is covered when we initialized everything in the dp array to 0
+        
+        // Precompute the palindrome status
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                isPal(i, j, s, isPaldp);
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+                int mini = INT_MAX;
+                for (int k = i; k < n; k++) {
+                    if (isPal(i, k, s, isPaldp)) {
+                        mini = min(mini, 1 + dp[k+1]);
+                    }
+                }
+                dp[i] = mini;
+        }
+        return dp[0]-1;
+    }
+};
---
+++ DP - Practice 2/Partition Array for Maximum Sum.cpp
@@ -0,0 +1,35 @@
+https://leetcode.com/problems/partition-array-for-maximum-sum/description/
+
+Dynamic programming -
+
+FRONT PARTITION LOGIC - see striver to understand more.
+
+Recurrence relation solution ->
+
+class Solution {
+public:
+    int solve(int i, int k, vector<int> &arr) {
+        if (i == 0) return 0;
+
+        int maxEle = 0;
+        int ans = 0;
+
+        for (int j = i; j >= max(1, i - k + 1); j--) {
+            maxEle = max(maxEle, arr[j - 1]);
+            int partitionSize = i - j + 1;
+
+            // Calculate the sum for this partition
+            int sumOfEles = maxEle * partitionSize;
+            int remaining = solve(j - 1, k, arr);
+
+            ans = max(ans, sumOfEles + remaining);
+        }
+        return ans;
+    }
+
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        return solve(n, k, arr);
+    }
+
+};
