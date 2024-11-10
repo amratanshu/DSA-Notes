8d7e1d99e47816ccd3e7c4ccb119b7f0f00c321d
Apple <amratanshu99@gmail.com>
2024-11-10 13:22:08 -0500

    changes

---
+++ Leetcode Random/String compression 1.cpp
@@ -0,0 +1,34 @@
+https://leetcode.com/problems/string-compression/
+
+My solution ->
+
+Basic pointer question - but writing the loop was a bit confusing.
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        if (chars.size() == 1) return 1;
+        
+        int newPointer = 0;
+        int i = 0;
+        while (i < chars.size()) {
+            char currentChar = chars[i];
+            int count = 0;
+            while (i < chars.size() && chars[i] == currentChar) {
+                count++;
+                i++;
+            }
+        
+            chars[newPointer++] = currentChar;
+        
+            if (count > 1) {
+                for (char c : to_string(count)) {
+                    chars[newPointer++] = c;
+                }
+            }
+        }
+        
+        chars.resize(newPointer); // Resize the vector to the new length
+        return newPointer;
+    }
+};
