bf0633c751de5028886bcf55e1fe48b327d8500c
Apple <amratanshu99@gmail.com>
2024-12-05 19:59:22 -0500

    changes

--- UNSOLVED/3362. Zero Array Transformation III.cpp
+++ UNSOLVED/3362. Zero Array Transformation III.cpp
@@ -7,3 +7,4 @@ inside wali query - can remove
 
 we need a big query that covers the biggest window in terms of low to high index.
 
+For these Zero Array Tranformation 1,2,3 - for all questions - also read up LINE SWEEP ALGORITHM!.
--- UNSOLVED/Unsolved questions.cpp
+++ UNSOLVED/Unsolved questions.cpp
@@ -4,3 +4,5 @@ Zero Array transformation 2, 3
 String compression 2, 3
 
 Minimize the Maximum Adjacent Element Difference -> Meet in the middle strategy (dont know this algorithm)
+
+LINE SWEEP ALGORITHM - zero array transformation me kaam aya hais
